513693163de4d873e7ead54a27a6cdfcf5cba3ad
Anoop Elias <anoopelias@gmail.com>
2014-12-29 11:43:50 +0530

    Added LESS support as well

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,26 @@ module.exports = function(grunt) {
         }
       }
     },
+    less: {
+      development: {
+        options: {
+          paths: ['less']
+        },
+        files: [
+          /* This might be useful when we start using imports,
+          http://stackoverflow.com/a/15600742/1328888
+
+          */
+          {
+            expand: true,
+            cwd: 'less',
+            src: ['*.less'],
+            dest: 'css/',
+            ext: '.css'
+          }
+        ]
+      }
+    },
     watch: {
       startup: {
           files: [],
@@ -31,6 +51,10 @@ module.exports = function(grunt) {
       karma: {
         files: ['js/**/*.js'],
         tasks: ['karma:unit:run']
+      },
+      less: {
+        files: ['less/**/*.less'],
+        tasks: ['less']
       }
     },
   });
@@ -38,4 +62,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-wiredep');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
 };
